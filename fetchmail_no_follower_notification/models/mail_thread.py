de846f51f25a47b8b43417a23c9ff0f1a269ab41
Henrik Norlin <henrik.norl@gmail.com>
2025-06-18 17:27:23 +0200

    [LOG] _notify_get_recipients with context fetchmail_cron_running

--- fetchmail_no_follower_notification/models/mail_thread.py
+++ fetchmail_no_follower_notification/models/mail_thread.py
@@ -1,4 +1,9 @@
+import logging
+
 from odoo import models
+
+
+_logger = logging.getLogger(__name__)
 
 
 class MailThread(models.AbstractModel):
@@ -9,6 +14,7 @@ class MailThread(models.AbstractModel):
         Do not notify followers if the context indicates that the fetchmail cron is running.
         """
         if self.env.context.get("fetchmail_cron_running"):
+            _logger.info("fetchmail_no_follower_notification due to fetchmail_cron_running.")
             return []
         else:
             return super()._notify_get_recipients(message, msg_vals, **kwargs)
