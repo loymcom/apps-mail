e1657bea5b734073453a07de443e0b6c847dc354
Henrik Norlin <henrik.norl@gmail.com>
2025-06-18 19:42:02 +0200

    fetchmail_no_follower_notification logging

--- fetchmail_no_follower_notification/__manifest__.py
+++ fetchmail_no_follower_notification/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Loym AS, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/mail",
     "category": "Social Network",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "license": "AGPL-3",
     "depends": ["mail"],
     "data": [
--- fetchmail_no_follower_notification/models/mail_thread.py
+++ fetchmail_no_follower_notification/models/mail_thread.py
@@ -14,7 +14,8 @@ class MailThread(models.AbstractModel):
         Do not notify followers if the context indicates that the fetchmail cron is running.
         """
         if self.env.context.get("fetchmail_cron_running"):
-            _logger.info("fetchmail_no_follower_notification due to fetchmail_cron_running.")
+            _logger.info("_notify_get_recipients: fetchmail_no_follower_notification due to fetchmail_cron_running.")
             return []
         else:
+            _logger.info("_notify_get_recipients: super()")
             return super()._notify_get_recipients(message, msg_vals, **kwargs)
