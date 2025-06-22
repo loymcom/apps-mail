06b54e0130512afe34592a40909a4f517a95f1f5
Henrik Norlin <henrik.norl@gmail.com>
2025-06-22 13:00:37 +0200

    log request params & headers

--- fetchmail_no_follower_notification/models/mail_thread.py
+++ fetchmail_no_follower_notification/models/mail_thread.py
@@ -2,6 +2,7 @@ import logging
 import traceback
 
 from odoo import models
+from odoo.http import request
 
 
 _logger = logging.getLogger(__name__)
@@ -14,23 +15,17 @@ class MailThread(models.AbstractModel):
         """
         Do not notify followers if the context indicates that the fetchmail cron is running.
         """
+        if request and hasattr(request, 'httprequest') and request.httprequest:
+            _logger.warning("request.params: " + str(request.params))
+            _logger.warning("request.httprequest.headers: " + str(request.httprequest.headers))
         # fetchmail_cron_running is not reliable, but default_fetchmail_server_id should be
         if self.env.context.get("default_fetchmail_server_id"):
-            _logger.warning("HENRIK: _notify_get_recipients: fetchmail_no_follower_notification due to fetchmail_cron_running.")
-            _logger.warning("Stack trace for _notify_get_recipients:")
+            _logger.warning("_notify_get_recipients: fetchmail_no_follower_notification due to fetchmail_cron_running.")
+            _logger.warning("_notify_get_recipients traceback:")
             _logger.warning(traceback.format_stack())
             return []
         else:
-            _logger.warning("HENRIK: _notify_get_recipients: context = " + str(self.env.context))
-            _logger.warning("Stack trace for _notify_get_recipients:")
+            _logger.warning("_notify_get_recipients: context = " + str(self.env.context))
+            _logger.warning("_notify_get_recipients traceback:")
             _logger.warning(traceback.format_stack())
             return super()._notify_get_recipients(message, msg_vals, **kwargs)
-
-    def message_update(self, msg_dict, update_vals=None):
-        """
-        Update the message with the given values.
-        """
-        _logger.warning("HENRIK: message_update: context = " + str(self.env.context))
-        _logger.warning("Stack trace for message_update:")
-        _logger.warning(traceback.format_stack())
-        return super().message_update(msg_dict, update_vals=update_vals)
