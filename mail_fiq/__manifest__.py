773b715975216a6d2dfa8ce1ed563baade9cd07c
Henrik Norlin <henrik.norl@gmail.com>
2025-06-25 16:18:37 +0200

    [ADD] mail_fiq

---
+++ mail_fiq/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2025 FIQ as
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "FIQ Mail Apps",
+    "author": "FIQ as, Odoo Community Association (OCA)",
+    "website": "https://fiq.no",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+
+        # OCA
+        "mail_composer_cc_bcc",
+        "mail_optional_follower_notification",
+        "mail_quoted_reply",
+        "mail_tracking",
+
+        # Loym/FIQ
+        "mail_force_email_notification_compose",
+        "mail_message_security",
+
+        # Wishlist
+        # - mail_quoted_reply_all
+        # - User setting: Handle important notifications in Email, otherwise in Odoo
+    ],
+    "data": [
+        "security/mail_message_security.xml",
+    ],
+}
