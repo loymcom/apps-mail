84833a4b3fd35c11f2a88ee6ddaaf17dcc345b1e
a-baade <andreasbaade@hotmail.com>
2025-07-16 14:33:44 +0200

    mail_message_security: group.user to .portal update to new version

--- mail_message_security/__manifest__.py
+++ mail_message_security/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Loym AS, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/mail",
     "category": "Social Network",
-    "version": "18.0.1.0.6",
+    "version": "18.0.1.0.7",
     "license": "AGPL-3",
     "depends": ["mail"],
     "data": [
